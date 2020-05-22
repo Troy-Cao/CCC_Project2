a2c8422b857f4728c75b3b12d351b479cf1d3e85
Chenqin Zhang <Renyqin@users.noreply.github.com>
2020-05-22 17:43:08 +1000

    real modifymetadata.py

--- shards/modifyMetadata.py
+++ shards/modifyMetadata.py
@@ -21,13 +21,16 @@ with open(input) as json_file:
 shards = []
 for shard in data["by_range"]:
     shards.append(shard)
-    data["by_range"][shard].append(newnode)
+    if newnode not in data["by_range"][shard]:
+        data["by_range"][shard].append(newnode)
 
 data["by_node"][newnode] = []
 
 for shard in shards:
-    data["changelog"].append(["add",shard, newnode])
-    data["by_node"][newnode].append(shard)
+    if ["add",shard, newnode] not in data["changelog"]:
+        data["changelog"].append(["add",shard, newnode])
+    if shard not in data["by_node"][newnode]:
+        data["by_node"][newnode].append(shard)
 
 with open(output, 'w') as outfile:
     json.dump(data, outfile)
