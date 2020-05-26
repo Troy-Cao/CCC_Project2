5f411309cdcafcf7187dbc9633d7b16675994bf7
Chenqin Zhang <Renyqin@users.noreply.github.com>
2020-05-27 00:33:52 +1000

    front-end

---
+++ WebDesign-Flask/README.md
@@ -0,0 +1,15 @@
+## May 15 大概整这样了
+`python test.py`
+
+![webpage](https://github.com/Troy-Cao/CCC_Project2/blob/frontEnd/_log/v1_web.png)
+
+
+---
+
+
+
+## echarts实现北京西城区地图Demo
+
+![image login](https://github.com/zhangqian00/echarts-map-xicheng/blob/master/img/demo.png)
+
+#### 由于地图json文件，是异步获取的，所以查看demo时，需要服务器访问方式查看
---
+++ WebDesign-Flask/index.py
@@ -0,0 +1,46 @@
+from flask import Flask, render_template
+import couchdb
+
+
+app = Flask(__name__)
+couch = couchdb.Server("http://admin:admin@172.26.130.241:5984/")
+db = couch['result']
+
+# Route - The Main Page
+@app.route("/")
+@app.route("/index")
+def index(): 
+    
+    return render_template("index.html")
+
+
+# Route - Senerios
+@app.route("/scenario1/<number>")
+def senario1(number):
+    # file_name = 
+    return render_template("scenario1.html")#, senario_id=number)
+    
+@app.route("/scenario2/<number>")
+def senario2(number):
+
+    return render_template("scenario2.html")#, senario_id=number)
+
+@app.route("/scenario3/<number>")
+def senario3(number):
+
+    return render_template("scenario3.html")#, senario_id=number)
+
+
+# Route - Database
+@app.route("/data/result/<result_id>")
+def result(result_id):
+    tmp = db[result_id]
+    return tmp
+
+@app.route("/data/aurin/<number>")
+def aurin(number):
+    return
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
